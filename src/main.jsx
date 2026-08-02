c3a454489d2c04956794e966166fd6cfb0388420
kammmartin7-rgb <kammmartin7@gmail.com>
2026-08-02 13:28:37 +0300

    Allow public demo routes without authentication

--- src/main.jsx
+++ src/main.jsx
@@ -7,16 +7,22 @@ import ProductionErrorBoundary from './components/ProductionErrorBoundary'
 import { findRecoveredPlumberLead, verifyLeadPersistence } from './services/leadPersistence'
 import { AuthProvider } from './context/AuthContext'
 import AuthGate from './components/AuthGate'
+import ShareableDemo from './components/WebsiteBuilder/ShareableDemo'
+import { parseShareableDemoRoute } from './components/WebsiteBuilder/demoStorage'
 
 if (import.meta.env.DEV) {
   window.__bsHunterVerifyLeadPersistence = verifyLeadPersistence
   window.__bsHunterFindPlumberLead = findRecoveredPlumberLead
 }
 
+const publicDemoRoute = parseShareableDemoRoute()
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ProductionErrorBoundary>
-      <AuthProvider><LanguageProvider><AuthGate><App /></AuthGate></LanguageProvider></AuthProvider>
+      {publicDemoRoute
+        ? <LanguageProvider><ShareableDemo route={publicDemoRoute} /></LanguageProvider>
+        : <AuthProvider><LanguageProvider><AuthGate><App /></AuthGate></LanguageProvider></AuthProvider>}
     </ProductionErrorBoundary>
   </StrictMode>,
 )
